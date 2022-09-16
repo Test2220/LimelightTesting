dc899b5a3376ab94907ea072c3fa747e928eaccb
SeanCh7 <62511636+SeanCh7@users.noreply.github.com>
2022-09-15 18:29:10 -0700

    Added treemap and return speed value (broken)
    Made by Tim, Henry, and Sarah

--- LimelightDistanceProject/src/main/java/frc/robot/Robot.java
+++ LimelightDistanceProject/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
@@ -11,18 +14,23 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+  }
 
   @Override
   public void robotPeriodic() {
@@ -30,34 +38,44 @@ public class Robot extends TimedRobot {
   }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+  }
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+  }
 
   @Override
-  public void teleopInit() {}
+  public void teleopInit() {
+  }
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+  }
 
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+  }
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+  }
 
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+  }
 
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+  }
 
   private final NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight-shooter");
   /* SUBSYSTEM CONSTRUCTOR */
@@ -65,15 +83,15 @@ public class Robot extends TimedRobot {
   /**
    * Limelight subsystem constructor. No arguments necessary or present.
    */
-      // Set camera mode to use vision processing
+  // Set camera mode to use vision processing
 
   public double getVOffset() {
     NetworkTableEntry targetY = limelight.getEntry("ty");
     double val = targetY.getDouble(0);
     return val;
-}
+  }
 
-public double getDistanceToTarget() {
+  public double getDistanceToTarget() {
     NetworkTableEntry ty = limelight.getEntry("ty");
     double targetOffsetAngle_Vertical = ty.getDouble(0.0);
     SmartDashboard.putNumber("targetOffsetAngleVertical", targetOffsetAngle_Vertical);
@@ -90,8 +108,51 @@ public double getDistanceToTarget() {
     SmartDashboard.putNumber("angleToGoalDegrees", angleToGoalDegrees);
     double angleToGoalRadians = Math.toRadians(angleToGoalDegrees);
 
-    //calculate distance
-    double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)/Math.tan(angleToGoalRadians);
+    // calculate distance
+    double distanceFromLimelightToGoalInches = (goalHeightInches - limelightLensHeightInches)
+        / Math.tan(angleToGoalRadians);
     return distanceFromLimelightToGoalInches;
-}
+  }
+
+  // public double flywheelRPM(){
+  // double distance = getDistanceToTarget();
+  // if (distance == 12) {
+
+  // }
+  // }
+  public double test() {
+    TreeMap<Double, Double> map = new TreeMap<>();
+    map.put(0.0, 50.0);
+    map.put(24.0, 100.0);
+    map.put(48.0, 150.0);
+    map.put(72.0, 200.0);
+    map.put(96.0, 250.0);
+    map.put(120.0, 300.0);
+    double distance = 12;
+
+    Double speed = map.get(distance);
+    if (speed == null) {
+      Entry<Double, Double> c = map.ceilingEntry(distance);
+      Entry<Double, Double> f = map.floorEntry(distance);
+      if (c == null) {
+        if (f == null) {
+          return 0;
+        } else {
+          return f.getValue();
+        }
+      } else {
+        if (f == null) {
+          return c.getValue();
+        } else {
+          double valuef = f.getValue();
+          double valuec = c.getValue();
+          double keyf = f.getKey();
+          double keyc = c.getKey();
+
+        }
+      }
+    } else {
+     return double speed = Double.parseDouble(speed);
+    }
+  }
 }
